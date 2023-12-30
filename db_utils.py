9b3542265765eb23efc768933852d0820ac34d2e
Jade-Why <jade.yamoah@googlemail.com>
2023-12-30 16:52:44 +0000

    Fixed engine issues, saves data into .csv file

--- db_utils.py
+++ db_utils.py
@@ -1,5 +1,7 @@
-import yaml
 from sqlalchemy import create_engine
+from sqlalchemy import inspect
+from sqlalchemy import text
+import yaml
 import psycopg2
 import pandas as pd
 
@@ -9,54 +11,24 @@ with open('credentials.yaml', 'r') as cred_file:
 class RDSDatabaseConnector:
     def __init__(self, db_connector):
         self.db_connector = db_connector
-        #print(self.db_connector['RDS_HOST'])
     
     def init_engine(self):
-        #self.db_connector
-        #DATABASE_TYPE = 'postgresql'
-        #DBAPI = 'psycopg2'
         engine = create_engine(f"postgresql://{self.db_connector['RDS_USER']}:{self.db_connector['RDS_PASSWORD']}@{self.db_connector['RDS_HOST']}:{self.db_connector['RDS_PORT']}/{self.db_connector['RDS_DATABASE']}")
-        #engine.execution_options(isolation_level='AUTOCOMMIT').connect()
-
-        #return engine
-        return engine.execution_options(isolation_level='AUTOCOMMIT').connect()
+        return engine
     
     def extract_data(self):
-        self.conn = self.init_engine()
-        
-        #self.conn = psycopg2.connect(   #figure out how to use init_engine here
-            #host=self.db_connector['RDS_HOST'],
-            #port=self.db_connector['RDS_PORT'],
-            #database=self.db_connector['RDS_DATABASE'],
-            #user=self.db_connector['RDS_USER'],
-            #password=self.db_connector['RDS_PASSWORD']
-        #)
-        cursor = self.conn.cursor()
+        db_engine = self.init_engine()
+        db_engine.execution_options(isolation_level='AUTOCOMMIT').connect()
 
-        select_query = "SELECT * FROM loan_payments"
+        sql_query = '''SELECT * FROM loan_payments'''
 
-        cursor.execute(select_query)
-        rows = cursor.fetchall()
-
-        self.column_names = self.conn.execute(select_query)
-        print(self.column_names)
-
-        #for row in rows:
-            #print(row)
-
-        cursor.close()
-        self.conn.close()
+        self.loan_df = pd.read_sql_query(sql_query, con=db_engine)
+            
     
-    def to_df(self):
-        #pass
-        loan_data = self.extract_data()
-        loan_data
+    def df_to_csv(self):
+        self.loan_df.to_csv("loans.csv")
 
-        #loan_df = pd.DataFrame(loan_data, columns)
-        
 
 rds_database_connector = RDSDatabaseConnector(db_connector)
-engine_initialised = rds_database_connector.init_engine()
 rds_database_connector.extract_data()
-#print(engine_initialised)
-#print(db_connector)
+rds_database_connector.df_to_csv()
