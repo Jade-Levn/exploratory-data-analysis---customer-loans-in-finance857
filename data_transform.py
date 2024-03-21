b47167174bdf67d8e583d4707f0ce120d0d7f11d
Jade <78552210+Jade-Why@users.noreply.github.com>
2024-03-21 18:31:14 +0000

    Adds and removes methods from dataframe_info.py and data_transform.py

--- data_transform.py
+++ data_transform.py
@@ -5,31 +5,19 @@ class DataTransform:
     """
     
     """
-    def __init__(self, dataset):
-        self.df = pd.read_csv(dataset)
-        pd.set_option('display.max_columns', None)
-
-    def show_rows(self, num_rows=5, head_or_tail='head'):
-        if head_or_tail == 'head':
-            return self.df.head(num_rows)
-        elif head_or_tail == 'tail':
-            return self.df.tail(num_rows)
-        
-        return self.df.head(num_rows)
-    
-    def get_info(self):
-        return self.df.info()
-
-    def to_categorical(self, column):
+    def to_categorical(self, df, column):
         self.df[column] = self.df[column].astype('category')
         return self.df
     
-    def format_dates(self, column):
+    def format_dates(self, df, column):
         self.df[column] = pd.to_datetime(self.df[column], format='%b-%Y')
         return self.df
 
-    def term_to_numeric(self, column):
+    def term_to_numeric(self, df, column):
         self.df[column] = self.df[column].astype('string')
         self.df[column] = self.df[column].str.extract('(\d+)', expand=False) \
             .astype(float)
         return self.df
+
+    def drop_column(self, df, column):
+        return self.df.drop(column, axis=1, inplace=True)
--- dataframe_info.py
+++ dataframe_info.py
@@ -1,7 +1,34 @@
 import pandas as pd
-
-
 class DataFrameInfo:
+    """
+    -Describe all columns in the DataFrame to check their data types /
+    -Extract statistical values: median, standard deviation and mean from the columns and the DataFrame X (not sure if I need this)
+    -Count distinct values in categorical columns/
+    -Print out the shape of the DataFrame X don't need this as I already know how many rows there are
+    -Generate a count/percentage count of NULL values in each column
+    -Any other methods you may find useful
+    """
+    def __init__(self, dataset):
+        self.df = pd.read_csv(dataset)
+        pd.set_option('display.max_columns', None)
     
-    def describe_columns(self, df):
-        return df.describe()
+    def show_rows(self, num_rows=5, head_or_tail='head'):
+        if head_or_tail == 'head':
+            return self.df.head(num_rows)
+        elif head_or_tail == 'tail':
+            return self.df.tail(num_rows)
+        
+        return self.df.head(num_rows)
+
+    def get_info(self):
+        return self.df.info()
+
+    def describe_columns(self):
+        return self.df.describe()
+
+    def count_categorical(self, column):
+        return self.df[column].value_counts()
+
+    def null_percentages(self):
+        print("Percentage of null values per column:")
+        return self.df.isnull().mean() * 100
